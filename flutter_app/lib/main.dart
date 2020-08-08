b88df6402770ad3b2ee7b7437d2299e83f973f97
DESKTOP-82143OB\jiaju <domo36k@gmail.com>
2020-08-08 17:47:01 +0800

    Add styling to widgets

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,31 +2,31 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text('my first app'),
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text(
-          'hello ninjas!',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower',
-          )
-        ),
+        child: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.alternate_email),
+          color: Colors.amber,
+        )
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Text('click'),
         backgroundColor: Colors.red[600],
       ),
-    ),
-  ));
+    );
+  }
 }
-
- 
